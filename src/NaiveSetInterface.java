98072d8b9401dc09e42f2fe60ded78ae3482801f
ekaterina <katya.levchenko.98@gmail.com>
2017-02-01 16:36:01 +0300

    Initial commit

--- src/NaiveSetInterface.java
+++ src/NaiveSetInterface.java
@@ -9,7 +9,17 @@ public interface NaiveSetInterface {
      */
     public void add(int value);
 
+    /**
+     * Shift 1 to left by value then binary NOT and AND with old number,
+     * so new number has 0 on right place
+     * @param value that we want to delete
+     */
     public void remove(int value);
 
+    /**
+     * Gets right bit from right number in array
+     * @param value that we want to check
+     * @return if there is 1 on right place
+     */
     public boolean isContain(int value);
 }
